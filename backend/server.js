8c0f2787c734cf5d2fe6f8f379d4afc257b4b7c6
DanMK <danielkatanti@gmail.com>
2025-03-27 10:27:42 +0200

    errHandler and Notfound middleware added !!!

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import studentApi from "./routes/studentRoute.js";
-const port = 4000;
+import dotenv from "dotenv";
+import { errorHandler, notFound } from "./middleware/errorHandler.js";
+dotenv.config();
+
+const port = process.env.PORT || 4000;
 
 const app = express();
 
 // route Api
 app.use("/api/student", studentApi);
+
+// middleware
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => res.send("Hi how are you today ?"));
 
