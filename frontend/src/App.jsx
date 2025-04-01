403d02de3c7fb5a3d13ad26cea9252af7f62ecd7
DanMK <danielkatanti@gmail.com>
2025-04-01 18:20:41 +0200

    frontend part set

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1>hi it's dan</h1>
+    </>
+  );
+}
+
+export default App;
