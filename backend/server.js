111bd97e74e35f3c570e285e31c6508241cf1ae8
DanMK <danielkatanti@gmail.com>
2025-05-05 21:36:58 +0200

    script addedd!!!

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import studentApi from "./routes/studentRoute.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -19,6 +20,20 @@ app.use(cookieParser());
 
 // route Api
 app.use("/api/student", studentApi);
+
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 // middleware for Errors
 app.use(notFound);
